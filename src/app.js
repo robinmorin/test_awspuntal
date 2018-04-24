c77be7e7c30d95057bf87303975616e21a220787
Robin Morin <robinmorin79@gmail.com>
2018-04-24 01:59:35 -0300

    Updating file app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+require('./routers/routers')(app);
 app.listen(app.get('port'), () => {
   console.log('server on port 3000');
 });
