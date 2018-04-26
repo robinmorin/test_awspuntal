a3d972bb3b9c3d71615f5b2428ee33bcc7617216
Robin Morin <robinmorin79@gmail.com>
2018-04-25 21:30:09 -0300

    routers.js

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -6,24 +6,24 @@ module.exports = function(app){
         res.status(200).json(data);
       });
     });
+
+    app.post('/dados', (req,res) => {
+      const dadosStruct = {
+        id: null,
+        sobrenome: req.body.sobrenome,
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        datanascimento: req.body.datanascimento
+      };
+
+      dados.insertDados(dadosStruct, (err,data) => {
+        if(data && data.InsertId){
+          res.json({
+            success: true,
+            msg: 'Dados Pessoais inseridos',
+            id: data.InsertId
+          })
+        }
+      })
+    });
 };
-
-app.post('/dados', (req,res) => {
-    const dadosStruct = {
-      id: null,
-      sobrenome: req.body.sobrenome,
-      nome: req.body.nome,
-      telefone: req.body.telefone,
-      datanascimento: req.body.datanascimento
-    };
-
-    dados.insertDados(dadosStruct, (err,data) => {
-      if(data && data.InsertId){
-        res.json({
-          success: true,
-          msg: 'Dados Pessoais inseridos',
-          id: data.InsertId
-        })
-      }
-    })
-});
